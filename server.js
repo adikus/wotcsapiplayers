a7b46c5facddccbf6daee0cb4509e2d3212ac25f
Andrej Hoos <andrej.hoos@gmail.com>
2013-08-06 09:52:01 +0200

    gzip support

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var cls = require("./lib/class"),
     _ = require("underscore"),
     http = require("http"),
-    Router = require("./router");
+    Router = require("./router"),
+    zlib = require('zlib');
 
 module.exports = server = cls.Class.extend({
 	init: function(port){
@@ -9,14 +10,14 @@ module.exports = server = cls.Class.extend({
 		var self = this;
 		
 		http.createServer(function (request, response) {
-			 
-		    response.writeHead(200, {
-		    	'Content-Type': 'text/plain',
-		    	'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-		    	'Access-Control-Allow-Credentials': true,
-		    	'Access-Control-Allow-Origin': '*',
-		    	'Access-Control-Allow-Headers': 'Content-Type, *'
-		    }); 
+			var acceptEncoding = request.headers['accept-encoding'];
+		  	if (!acceptEncoding) {
+		    	acceptEncoding = '';
+		  	}
+		  	var useGzip = acceptEncoding.match(/\bgzip\b/);
+		  	
+		  	response.writeHead(200, self.responseHeaders(useGzip));			
+			
 		    self.data = '';
 		    request.on('data', function(chunk) {
 				self.data += chunk.toString('utf8');
@@ -29,14 +30,42 @@ module.exports = server = cls.Class.extend({
 		    	
 		    	if(typeof responseData == 'function'){
 		    		responseData(function(finalResponseData){
-		    			response.end(JSON.stringify(finalResponseData, null, "\t"));
+		    			self.endResponse(response, JSON.stringify(finalResponseData, null, "\t"), useGzip);
 		    		});
 		    	}else{
-		    		response.end(JSON.stringify(responseData, null, "\t"));
+		    		self.endResponse(response, JSON.stringify(responseData, null, "\t"), useGzip);
 		    	}
 		    });
 		}).listen(port);   
 		console.log('Server running on port '+port);
+	},
+	
+	responseHeaders: function(useGzip){
+		var headers = {
+		    	'Content-Type': 'text/plain',
+		    	'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+		    	'Access-Control-Allow-Credentials': true,
+		    	'Access-Control-Allow-Origin': '*',
+		    	'Access-Control-Allow-Headers': 'Content-Type, *'
+		    }
+		if(useGzip){
+			headers['content-encoding'] = 'gzip';
+		}
+		return headers;
+	},
+	
+	endResponse: function(response, data, useGzip){
+		if (useGzip) {
+			zlib.gzip(data, function(err, buffer){
+				if (!err) {
+					response.end(buffer);
+				}else{
+					response.end(JSON.stringify(err, null, "\t"));
+				}
+			});
+		}else{
+			response.end(data);
+		}
 	},
 
 	setRoute: function(route,callback){
