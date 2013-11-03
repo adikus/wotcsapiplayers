843720aa935443ebf7c0c10b30f948aecc359ae4
Andrej Hoos <andrej.hoos@gmail.com>
2013-11-03 11:12:52 +0000

    Finnish debuging - auth fail

--- db_types.js
+++ db_types.js
@@ -1,22 +1,21 @@
 var mongoose = require('mongoose'),
     Config = require("./config");
 
-console.log(process.env.MONGOHQ_URL || Config.db.defHost);
 var oldDB = mongoose.createConnection(process.env.MONGOHQ_URL || Config.db.defHost,function(err){
 	if (err){
-		console.log("Error MONGOHQ DB");
+		console.log("Error MONGOHQ DB", err);
 		throw err;
 	}else console.log("Connected to MONGOHQ DB");
 });
 var playerDB = mongoose.createConnection(process.env.WOTCS_PLAYERDB,function(err){
 	if (err){
-		console.log("Error MONGOLAB player DB");
+		console.log("Error MONGOLAB player DB", err);
 		throw err;
 	} else console.log("Connected to MONGOLAB player DB");
 });
 var clanDB = mongoose.createConnection(process.env.WOTCS_CLANDB,function(err){
 	if (err){
-		console.log("Error MONGOLAB clan DB");
+		console.log("Error MONGOLAB clan DB", err);
 		throw err;
 	} else console.log("Connected to MONGOLAB clan DB");
 });
